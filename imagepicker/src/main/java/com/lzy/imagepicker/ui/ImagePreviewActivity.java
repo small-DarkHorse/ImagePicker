707e65d2f2f258397982960c0b5eda4c99b0a3fa
wzk <xiaokai.dana@foxmail.com>
2022-01-04 10:21:28 +0800

    Update ImagePreviewActivity.java

--- imagepicker/src/main/java/com/lzy/imagepicker/ui/ImagePreviewActivity.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/ui/ImagePreviewActivity.java
@@ -21,10 +21,10 @@ import com.lzy.imagepicker.view.SuperCheckBox;
 
 /**
  * ================================================
- * 作    者：jeasonlzy（廖子尧 Github地址：https://github.com/jeasonlzy0216
+ * 作    者：wangzhenkai
  * 版    本：1.0
  * 创建日期：2016/5/19
- * 描    述：
+ * 描    述：适配横竖屏切换
  * 修订历史：
  * ================================================
  */
