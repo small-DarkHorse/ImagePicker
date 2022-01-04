364b250e74887a3325fb226258a7451059587db7
wzk <xiaokai.dana@foxmail.com>
2022-01-04 10:11:44 +0800

    Update FolderPopUpWindow.java

--- imagepicker/src/main/java/com/lzy/imagepicker/view/FolderPopUpWindow.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/view/FolderPopUpWindow.java
@@ -19,7 +19,7 @@ import com.lzy.imagepicker.R;
 
 /**
  * ================================================
- * 作    者：jeasonlzy（廖子尧）Github地址：https://github.com/jeasonlzy0216
+ * 作    者：wangzhenkai
  * 版    本：1.0
  * 创建日期：16/8/1
  * 描    述：
